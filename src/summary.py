e973cbb81ceaa47fb32ae102600be1ab7e77050b
kubeqer <jakpluta@gmail.com>
2024-10-14 22:52:18 +0200

    change summary.py

--- src/summary.py
+++ src/summary.py
@@ -1,8 +1,11 @@
-from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
+from llama_index.core import (
+    Settings,
+    DocumentSummaryIndex,
+)
+from llama_index.core.node_parser import SentenceSplitter
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.llms.ollama import Ollama
 from llama_index.readers.file import PDFReader
-from sympy.logic.utilities import load_file
 
 from consts import DOCS_DIR
 from data_management import save_summary
@@ -16,13 +19,18 @@ def summary(doc: str, model: str, prompt: str) -> str:
     {prompt}
     [/INST]"""
     document = PDFReader(return_full_document=True).load_data(DOCS_DIR / doc)
-    Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-base-en-v1.5")
-    Settings.llm = Ollama(model=model, request_timeout=360.0)
-    index = VectorStoreIndex.from_documents(
+    index = DocumentSummaryIndex.from_documents(
         document,
+        embed_model=HuggingFaceEmbedding(model_name="BAAI/bge-base-en-v1.5"),
+        llm=Ollama(model=model, request_timeout=360.0, temperature=0),
+        show_progress=True,
     )
 
-    query_engine = index.as_query_engine()
+    query_engine = index.as_query_engine(response_mode="tree_summarize", use_async=True)
+    auto_summary = index.get_document_summary(document[0].doc_id)
+    print(f"SUMMARY:\n{str(auto_summary)}")
     response = str(query_engine.query(complete_prompt))
+
+
     save_summary(response, doc)
     return response
